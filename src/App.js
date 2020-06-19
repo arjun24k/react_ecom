1548cb5e0a27d67e9376f5373c206272680dfe8e
Arjun MK <arjunmkp18@gmail.com>
2020-06-19 23:43:55 +0530

    added basic skeleton of genres

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import HomePage from './homepage.component';
+import HomePage from './HomePage/homepage.component';
 
 class App extends React.Component {
-  constructor(){
-    super();
-    this.state={
-
-    }
-  }
 
   render(){
     return(

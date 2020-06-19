ff231342213c5565830f8093852f3691414ae292
Arjun MK <arjunmkp18@gmail.com>
2020-06-19 23:29:06 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import HomePage from './homepage.component';
+
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state={
+
+    }
+  }
+
+  render(){
+    return(
+      <HomePage/>
+    );
+  }
+
+  
+}
+
+export default App;
